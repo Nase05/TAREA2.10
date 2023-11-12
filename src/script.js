def268aec03d5cc3d06e75f779a2178df8e91a77
Nase05 <nerea3579@gmail,com>
2023-11-12 23:55:23 +0100

    Fix funcionalidad buscar

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    console.log("Script original cargado");
+// script.js (modificado para búsqueda)
+console.log("Script modificado cargado");
+
+// Nueva funcionalidad: Búsqueda de elementos en el DOM
+const buscarElemento = (selector) => {
+    const elemento = document.querySelector(selector);
+    return elemento ? elemento : "Elemento no encontrado";
+};
+
+// Ejemplo de uso
+const resultado = buscarElemento(".mi-clase");
+console.log(resultado);
+
 });
