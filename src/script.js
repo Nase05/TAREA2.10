75b195d0e70acf53cfe363040f00c52fa5bf87e2
Nase05 <nerea3579@gmail,com>
2023-11-13 00:01:54 +0100

    fix cambiar contenido

--- src/script.js
+++ src/script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    console.log("Script original cargado");
+    // script.js (modificado con funcionalidad adicional)
+console.log("Script modificado cargado");
+
+// Nueva funcionalidad: Búsqueda de elementos en el DOM
+const buscarElemento = (selector) => {
+    const elemento = document.querySelector(selector);
+    return elemento ? elemento : "Elemento no encontrado";
+};
+
+// Ejemplo de uso
+const resultado = buscarElemento(".mi-clase");
+console.log(resultado);
+
+// Otra funcionalidad: Cambiar dinámicamente el contenido de un elemento
+const cambiarContenido = (selector, nuevoContenido) => {
+    const elemento = buscarElemento(selector);
+    if (elemento !== "Elemento no encontrado") {
+        elemento.textContent = nuevoContenido;
+    }
+};
+
+// Ejemplo de uso
+cambiarContenido("#mi-elemento", "¡Nuevo Contenido!");
+
 });
